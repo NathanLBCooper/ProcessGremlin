bd29fae415e2034c36aa8e0f7758afa536aa65be
NathanC <nathan.lb.cooper@outlook.com>
2015-08-06 10:18:33 +0100

    split up class libary and console app

---
+++ ProcessGremlinApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProcessGremlin;
+
+namespace ProcessGremlinApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var gremlins = new GremlinBuilder();
+            var finders = new FinderBuilder();
+            var timerBuilder = new GremlinTimerBuilder();
+
+            var random = new Random();
+
+            using (
+                var gremlinTimer =
+                    timerBuilder.CreateGremlinTimer(
+                        finders.ConcatFinders(new[] { finders.GetNameBasedFinder("notepad"), finders.GetNameBasedFinder("python") }),
+                        gremlins.KillRandom(random),
+                        1000))
+            {
+                gremlinTimer.Start();
+                Console.ReadKey();
+            }
+        }
+    }
+}
