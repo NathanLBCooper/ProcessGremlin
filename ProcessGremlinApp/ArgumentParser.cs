fe2b84735bfd9ba728f807adfc18897db77cf5bb
NathanLBCooper <nathan.lb.cooper@outlook.com>
2016-05-06 10:33:27 +0100

    Fix crashing in the case of empty args

--- ProcessGremlinApp/ArgumentParser.cs
+++ ProcessGremlinApp/ArgumentParser.cs
@@ -6,21 +6,24 @@ namespace ProcessGremlinApp
     {
         public bool TryParse(string[] args, out Arguments arguments)
         {
-            string invokedVerb = null;
-            object invokedVerbInstance = null;
-
-            var options = new Options();
-            if (Parser.Default.ParseArguments(
-                args,
-                options,
-                (verb, subOptions) =>
-                {
-                    invokedVerb = verb;
-                    invokedVerbInstance = subOptions;
-                }) && invokedVerbInstance is CommonOptions)
+            if (args != null && args.Length != 0)
             {
-                arguments = new Arguments(invokedVerb, invokedVerbInstance);
-                return true;
+                string invokedVerb = null;
+                object invokedVerbInstance = null;
+
+                var options = new Options();
+                if (Parser.Default.ParseArguments(
+                    args,
+                    options,
+                    (verb, subOptions) =>
+                    {
+                        invokedVerb = verb;
+                        invokedVerbInstance = subOptions;
+                    }) && invokedVerbInstance is CommonOptions)
+                {
+                    arguments = new Arguments(invokedVerb, invokedVerbInstance);
+                    return true;
+                }
             }
 
             arguments = null;
